67d07c3e335217c620a56ade8b8e77aa562b726f
jvmr535 <46688620+jvmr535@users.noreply.github.com>
2020-07-17 20:01:18 -0300

    Passo 2

---
+++ airbnb-server/app/Controllers/Http/PropertyController.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const Property = use('App/Models/Property')
+
+class PropertyController {
+
+  async index ({ request, response, view }) {
+    const properties = Property.all()
+
+    return properties
+  }
+
+  async store ({ request, response }) {
+  }
+
+  async show ({ params }) {
+    const property = await Property.findOrFail(params.id)
+  
+    await property.load('images')
+  
+    return property
+  }
+
+  async update ({ params, request, response }) {
+  }
+
+  
+  async destroy ({ params, auth, response }) {
+    const property = await Property.findOrFail(params.id)
+  
+    if (property.user_id !== auth.user.id) {
+      return response.status(401).send({ error: 'Not authorized' })
+    }
+  
+    await property.delete()
+  }
+  
+}
+
+module.exports = PropertyController
